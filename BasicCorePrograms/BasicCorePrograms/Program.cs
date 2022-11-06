92d9fde789bcc6f0e0d5783c9330bdb799e98fa5
Manoj Kumar Manukonda <manojkumarmanukonda77@gmail.com>
2022-11-06 22:19:22 +0530

    [Manoj] Updated: Program.cs

--- BasicCorePrograms/BasicCorePrograms/Program.cs
+++ BasicCorePrograms/BasicCorePrograms/Program.cs
@@ -14,12 +14,31 @@ namespace BasicCorePrograms
                 switch (check)
                 {
                     case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
                         SwapTwoNumbers swapTwoNumbers = new SwapTwoNumbers();
                         swapTwoNumbers.Swap2Numbers();
+                        break;
+                    case 8:
+                        break;
+                    case 9:
+                        break;
+                    case 10:
                         break;
                     default:
                         break;
                 }
+                break;
             }
         }
     }
