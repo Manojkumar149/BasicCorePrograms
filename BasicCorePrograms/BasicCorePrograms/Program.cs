7a386cd7c855423cbef443e13bc11475de5ad10f
Manoj Kumar Manukonda <manojkumarmanukonda77@gmail.com>
2022-11-06 21:46:39 +0530

    [Manoj] Add: Folder Structure Created

---
+++ BasicCorePrograms/BasicCorePrograms/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace BasicCorePrograms
+{
+    public class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to Basic Core Programs");
+        }
+    }
+}
